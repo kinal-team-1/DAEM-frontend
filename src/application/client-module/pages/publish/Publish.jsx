5d4ba45ff5ac565e562415ecd498cb0eabecfbe9
jcastillo21 <bcastillo-2022474@kinal.edu.gt>
2024-07-19 15:48:18 -0600

    fixing resizing

--- src/application/client-module/pages/publish/Publish.jsx
+++ src/application/client-module/pages/publish/Publish.jsx
@@ -5,8 +5,8 @@ export function Publish() {
   const { LL } = useLocaleService();
 
   return (
-    <div className="flex flex-col md:grid grid-cols-2 gap-5 h-full">
-      <div className="p-4 bg-black/60 text-white h-full flex flex-col justify-between gap-5 px-10 text-xl">
+    <div className="flex flex-col md:grid grid-cols-2 gap-5 h-full min-h-fit">
+      <div className="p-4 bg-black/60 text-white  shrink-0 flex flex-col justify-between gap-5 px-10 text-xl">
         <div className="flex flex-col gap-5 grow justify-center">
           <p>{LL?.PAGES.PUBLISH.PARAGRAPH_ONE()}</p>
           <p>{LL?.PAGES.PUBLISH.PARAGRAPH_TWO()}</p>
