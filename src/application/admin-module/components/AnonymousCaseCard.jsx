3ec04231442a8f0e7f6ee6787bcdb0a37f00a509
jjimenez2022152 <jjimenez-2022152@kinal.edu.gt>
2024-07-20 18:25:04 -0600

    DAEM-20 #comment Se termina los 3 archivos de casps anonimos #time 1h

--- src/application/admin-module/components/AnonymousCaseCard.jsx
+++ src/application/admin-module/components/AnonymousCaseCard.jsx
@@ -1,5 +1,5 @@
 import { Link, useParams } from "react-router-dom";
-import { Image } from "../../../../components/Image";
+import { Image } from "../../components/Image";
 
 export function AnonymousCaseCard({
   id,
