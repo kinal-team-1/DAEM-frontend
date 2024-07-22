f3550d41ff390e8f2ecaa31a00d0b6422ee52288
jjimenez2022152 <jjimenez-2022152@kinal.edu.gt>
2024-07-21 23:44:16 -0600

    DAEM-20 #comment Se termina listar de casos anonimos #time 1h

--- src/routes/admin-routes.jsx
+++ src/routes/admin-routes.jsx
@@ -1,6 +1,12 @@
+import { AnonymousCase } from "../application/admin-module/anonymous-case/anonymous-case";
+
 export const adminRoutes = /** @type import("react-router-dom").RouteObject */ [
   {
     path: "",
     element: <div>Hi</div>,
+  },
+  {
+    path: "anonymous-case",
+    element: <AnonymousCase />,
   },
 ];
--- src/routes/user-routes.jsx
+++ src/routes/user-routes.jsx
@@ -3,6 +3,7 @@ import { Publish } from "../application/client-module/pages/publish/Publish";
 import { PublicCaseById } from "../application/client-module/pages/public-case-by-id/PublicCaseById";
 import { PublicCase } from "../application/client-module/pages/public-case/publicCase";
 import { EditProfile } from "../application/client-module/pages/edit-profile/EditProfile";
+import { AnonymousCase } from "../application/admin-module/anonymous-case/anonymous-case";
 
 export const userRoutes = /** @type import("react-router-dom").RouteObject */ [
   {
@@ -24,5 +25,9 @@ export const userRoutes = /** @type import("react-router-dom").RouteObject */ [
   {
     path: "edit-profile",
     element: <EditProfile />,
+  },
+  {
+    path: "anonymous-case",
+    element: <AnonymousCase />,
   },
 ];
