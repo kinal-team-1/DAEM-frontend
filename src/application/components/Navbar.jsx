24ccdbf943e906fa665173363648e8f419ce3861
lmonterroso2022135 <lmonterroso-2022135@kinal.edu.gt>
2024-07-22 05:05:13 -0600

    DAEM-22 #comment menu var, fondos y datos personales #time 1h

--- src/application/components/Navbar.jsx
+++ src/application/components/Navbar.jsx
@@ -1,12 +1,17 @@
+import { useState } from "react";
 import { Link, useLocation, useNavigate, useParams } from "react-router-dom";
 import { DropDown } from "./DropDown";
 import { SUPPORTED_LANGUAGES } from "../../config";
 import logo from "../../assets/logo.png";
 import { useLocaleService } from "../../services/locale";
+import { useAuthService } from "../../services/auth";
 
 export function Navbar() {
   const { locale } = useLocaleService();
-
+  // /////////////////////////////////////////////////////////////////////////////////
+  // Las partes comentadas del navbar se deben a que al usar el navbar en el login,
+  // causa un error porque este ocupa informacion del usuario logueado
+  // /////////////////////////////////////////////////////////////////////////////////
   return (
     <div className="flex py-2 px-3 gap-2 justify-between items-center bg-[#1b1a1a] text-white z-10">
       <div className="flex gap-3">
@@ -38,6 +43,19 @@ function TopBarButtons() {
   const navigate = useNavigate();
   const location = useLocation();
   const currentPath = location.pathname;
+  // const { user } = useAuthService();
+
+  // const iName = user.name ? user.name[0] : '';
+  // const iLastname = user.lastname ? user.lastname[0] : '';
+
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
+  const handleLogout = () => {
+    // localStorage.removeItem("token");
+  };
 
   return (
     <div className="flex gap-2 items-center text-sm">
@@ -68,10 +86,24 @@ function TopBarButtons() {
       />
       <button
         type="button"
-        className="border px-2 py-1 size-[calc(120px/3)] cursor-pointer rounded-full"
+        onClick={toggleDropdown}
+        className="border bg-black px-2 py-1 size-[calc(120px/3)] cursor-pointer rounded-full"
       >
-        <span className="text-sm">LC</span>
+        {/* <span className="text-sm font-bold">{iName}{iLastname}</span> */}
       </button>
+      {dropdownOpen && (
+        <div className="absolute z-20 right-0 mt-32 mr-2 w-52 bg-white border shadow-lg border-none">
+          <Link
+            to={`/${locale}/edit-profile`}
+            className="bg-vulcan-950  block p-3 text-md hover:bg-gray-600  font-bold"
+          >Profile</Link>
+          <Link
+            to={`/${locale}/login`}
+            onClick={handleLogout}
+            className="bg-vulcan-950 block p-3 text-md hover:bg-red-500 font-bold"
+          >Log out</Link>
+        </div>
+      )}
     </div>
   );
 }
